dffda651065f8ae662eea4f5c85a0453542f52a1
Mike_Kuzzz <mike.kmv@gmail.com>
2019-11-03 13:19:19 +0300

    BubleSortRevers WORK commit

--- src/ru/mike/BubleSortRevers.java
+++ src/ru/mike/BubleSortRevers.java
@@ -1,4 +1,28 @@
 package ru.mike;
 
+import java.util.Arrays;
+
 public class BubleSortRevers {
+    public static void main(String[] args) {
+        int[] arr = {34, 44, 2, 6, 32, 8, 76, 5, 7};
+        /*for (int barier = arr.length - 1; barier >= 0; barier--){
+            for (int index = 0; index < barier; index++){
+                if(arr[index] > arr[index + 1]){
+                    int tmp = arr[index];
+                    arr[index] = arr[index + 1];
+                    arr[index + 1] = tmp;
+                }
+            }
+        }*/
+        for (int barier = 0; barier <= arr.length - 1 ; barier++){
+            for (int index = arr.length -1; index > barier; index--){
+                if(arr[index] < arr[index - 1]){
+                    int tmp = arr[index];
+                    arr[index] = arr[index - 1];
+                    arr[index - 1] = tmp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
 }
